6acfe60746099205a96fc73256b7d5ee11d455c1
Leonardo Campos <leocampos.995@gmail.com>
2025-06-09 12:06:20 -0300

    ajust env

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
+import "dotenv/config"; 
+const PRODUCTION_ENVIRONMENT = process.env.PRODUCTION_ENVIRONMENT
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
   {
 		rules: {
-			"no-console": "óff",
+			"no-console": PRODUCTION_ENVIRONMENT ? "error" : "warn",
 			"no-undef": "warn",
 		},
 	},
